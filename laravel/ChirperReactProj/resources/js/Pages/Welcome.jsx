595792188872f70752c2f201138731d558705454
Ruslan Issapov <galinskirus@gmail.com>
2023-08-08 18:08:12 +0600

    created laravel proj on documintation

---
+++ laravel/ChirperReactProj/resources/js/Pages/Welcome.jsx
@@ -0,0 +1,50 @@
+import { Link, Head } from "@inertiajs/react";
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
+
+export default function Welcome({ auth, laravelVersion, phpVersion }) {
+    return (
+        <>
+            <Head title="Главная" />
+            <div className="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-gray-100 selection:text-white">
+                <div className="sm:fixed sm:top-0 sm:right-0 p-6 text-right">
+                    {auth.user ? (
+                        <>
+                            <Link
+                                style={{ marginRight: "10px" }}
+                                href={route("dashboard")}
+                                className="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
+                            >
+                                Dashboard
+                            </Link>
+                            <Link
+                                className="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
+                                href={route("chirps.index")}
+                            >
+                                chirps
+                            </Link>
+                        </>
+                    ) : (
+                        <>
+                            <Link
+                                href={route("login")}
+                                className="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
+                            >
+                                Log in
+                            </Link>
+
+                            <Link
+                                href={route("register")}
+                                className="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
+                            >
+                                Register
+                            </Link>
+                        </>
+                    )}
+                </div>
+                <div>
+                    
+                </div>
+            </div>
+        </>
+    );
+}
